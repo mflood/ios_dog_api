dcd9bb520f49fc4ba87405b5d1a10850d71b2c42
mflood <hermannwest@gmail.com>
2023-02-17 10:53:33 -0700

    Implement picker delegate methods using static list of breeds

--- Dog Api/Dog Api/ViewController.swift
+++ Dog Api/Dog Api/ViewController.swift
@@ -7,12 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
 
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var image: UIImageView!
+    
+    let breedList: [String] = ["hound", "pitt", "terrier"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.breedList.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.breedList[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("\(self.breedList[row])")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
         // Do any additional setup after loading the view.
     }
+    
+    
 
 
 }
